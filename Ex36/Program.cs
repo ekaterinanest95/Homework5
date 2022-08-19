87782a866a968b01c0d18b32dc928bb6158e23a4
Ekaterina <katya.kinal@gmail.com>
2022-08-19 13:05:49 +0300

    Ex36

---
+++ Ex36/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] array = new int[7];
+int sumOfOddNumbers = 0;
+
+int GetSumOfOddNumbers(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] % 2 != 0)
+        {
+           sumOfOddNumbers += array[i]; 
+        }
+
+    }
+    return sumOfOddNumbers;
+
+}
+
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-100, 100);
+}
+
+sumOfOddNumbers = GetSumOfOddNumbers(array);
+Console.WriteLine($"В массиве [{String.Join(' ', array)}] сумма нечетных чисел равна {sumOfOddNumbers}");
+
+
