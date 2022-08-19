7d14589f0564eb3ad9253b88a205d936c610f583
Ekaterina <katya.kinal@gmail.com>
2022-08-19 12:43:50 +0300

    Ex34

---
+++ Ex34/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] Array = new int[8];
+int count = 0;
+
+for(int i = 0; i < Array.Length; i++)
+{
+    Array[i] = new Random().Next(100, 1000);
+    if(Array[i] % 2 == 0) count +=1;
+
+}
+
+Console.WriteLine($"В массиве [{String.Join(' ', Array)}] {count} положительных чисел");
+
+
+
